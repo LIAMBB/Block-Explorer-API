5f39636975768c8c5b853e2be476620575d7aac9
LiamBB <liam@eastcode.ca>
2023-10-24 01:29:54 -0300

    debugging

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 const (
 	rpcUser     = "rpc"
 	rpcPass     = "rpc"
-	electrumURL = "172.17.0.1:50002"
+	electrumURL = "172.17.0.1:50001"
 	coreURL     = "http://localhost"
 	walletURL   = "/wallet/bank" // bank wallet for regtest use
 	nmcPort     = 18443
